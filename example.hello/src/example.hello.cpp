b27052b3323d63e290fde0e23b0f7253879a5092
rirfan <rirfan@ryerson.ca>
2015-10-26 16:24:47 -0400

    Added a new line to the program

---
+++ example.hello/src/example.hello.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : hello.cpp
+// Author      : Sabah
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	cout <<"Rakia irfan\n"<< endl;
+	return 0;
+}
